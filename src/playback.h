05d54c29aeec4c2c7174e94a065ade1066027b17
ddorstijn <dannydorstijn1997@gmail.com>
2020-09-26 23:14:44 +0200

    Cleaned code to playback

--- src/playback.h
+++ src/playback.h
@@ -15,8 +15,4 @@ struct metadata
 struct metadata* init_audio(const char* audio_file);
 void free_audio(struct metadata* md);
 
-int play(struct metadata* metadata);
-void write_callback(struct SoundIoOutStream *outstream, int frame_count_min, int frame_count_max);
-void underflow_callback(struct SoundIoOutStream *outstream);
-
 #endif
