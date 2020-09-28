7200faeba22e0f1298860a57957b00ac5febcbfd
ddorstijn <dannydorstijn1997@gmail.com>
2020-09-28 15:10:36 +0200

    Fixed a couple of errors after renaming songs folder

--- generate.sh
+++ generate.sh
@@ -5,6 +5,8 @@ tests=(128 512 1024 2048 8192 48000)
 for i in "${tests[@]}"
 do
 	echo $i
-	./bpm/bpm songs/sine_beat.wav "$i"
+	./bin/bpm data/sine_beat.wav "$i"
+	echo "Finished generating fft for audio fragment"
 	python tools/plot.py "$i"
+	echo "Finished generating plot from data"
 done
